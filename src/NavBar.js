35a3996611ed3e2acd9bab385a01c6257bdc95a5
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-05-25 10:08:07 -0400

    implemented location context cleaning when returning to homescreen or using nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { LocationContext } from "./LocationContext"
+import Button from '@material-ui/core/Button';
+import { useContext } from 'react';
 
 export default function Navbar() {
   const location = useLocation();
+  const { zip, setZip, lat, setLat, lng, setLng, coords, setCoords } = useContext(LocationContext);
+  const history = useHistory()
+  const cleanContext = () => {
+    setZip(0)
+    setLat(null)
+    setLng(null)
+    setCoords(null)
+  }
   return (
     <div
       style={{
@@ -12,14 +23,36 @@ export default function Navbar() {
         width: "30%"
       }}
     >
-      <Link to="/">Home </Link>
+      <Button
+        onClick={() => {
+          cleanContext()
+          history.push("/")
+        }}
+      >
+        Home
+        </Button>
       {location.pathname.includes("/restaurants") ? (
-        <Link to="/weather/null">Check Weather </Link>
+        <Button
+          onClick={() => {
+            cleanContext()
+            history.push("/weather/null")
+          }}
+        >
+          Check Weather
+        </Button>
       ) : (
         <div> Check Weather</div>
       )}
       {location.pathname.includes("/weather") ? (
-        <Link to="/restaurants/null/null">Search For Restaurants </Link>
+        <Button
+          onClick={() => {
+            cleanContext()
+            history.push("/restaurants/null/null")
+
+          }}
+        >
+          Search For Restaurants
+        </Button>
       ) : (
         <div> Search For Restaurants</div>
       )}
