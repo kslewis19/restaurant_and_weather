e5ad6a5ddf20c0ee332af9e799c54fa9268ac42c
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-05-25 15:46:38 -0400

    small changes to weather app

--- src/weatherComponents/HourlyWeatherCard.js
+++ src/weatherComponents/HourlyWeatherCard.js
@@ -1,15 +1,15 @@
 import { FaSun, FaCloudRain, FaCloud } from "react-icons/fa";
 
-function HourlyWeatherCard(props){
-    if (props.index!=0){
-        return(
-    
-            <div style={{borderWidth:5, backgroundColor: "lightblue", width:"40%", shadowColor: "black",shadowOpacity: 5.8,shadowRadius: 2,shadowOffset: {height: 100,width: 100 }}} >
+function HourlyWeatherCard(props) {
+    if (props.index != 0) {
+        return (
+
+            <div style={{ borderWidth: 5, backgroundColor: "lightgray", color: "black", width: "40%", shadowColor: "black", shadowOpacity: 5.8, shadowRadius: 2, shadowOffset: { height: 100, width: 100 } }} >
                 <h3> Hours From Now: {props.index}</h3>
                 <p>Temperature: {props.hour.temp}</p>
-                <p>Skys: {(props.hour.weather[0].main)} {(props.hour.weather[0].main)=="Clear"? <FaSun style={{color:"yellow"}}/>: (props.hour.weather[0].main)=="Clouds"? <FaCloud style={{color:"white"}}/>: (props.hour.weather[0].main)=="Rain"? <FaCloudRain style={{color:"blue"}}/>:"!"}</p>
-                
-            
+                <p>Skys: {(props.hour.weather[0].main)} {(props.hour.weather[0].main) == "Clear" ? <FaSun style={{ color: "yellow" }} /> : (props.hour.weather[0].main) == "Clouds" ? <FaCloud style={{ color: "white" }} /> : (props.hour.weather[0].main) == "Rain" ? <FaCloudRain style={{ color: "blue" }} /> : "!"}</p>
+
+
             </div>
         )
     }
